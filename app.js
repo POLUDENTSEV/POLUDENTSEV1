3bb39924b1413fb2db5f49d205e5f713309da61d
nikitapoludencev <poludencevnikita@gmail.com>
2024-05-20 02:31:55 +0300

    Добавлены routes

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const fs = require("fs").promises;
 const path = require("path");
 
-const app = express();
+const mainRoute = require('./routes/main')
+const gamesRoute = require('./routes/games')
 
 const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mainRoute, gamesRoute);
 
 app.listen(PORT, () => {
     console.log(`Приложение запущено тут: http://localhost:${PORT}`);
 });
 
-app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/', (req, res) => {
-    fs.readFile("./public/index.html", "utf-8").then((data) => {
-res.header("Content-Type", "text/html");
-res.send(data);
-});
-});
---
+++ routes/games.js
@@ -0,0 +1,11 @@
+     const gamesRouter = require("express").Router();
+     
+     const getAllGames = async (req, res) => {
+ 
+     };
+     
+     
+     gamesRouter.get("/games", getAllGames)
+     
+     module.exports = gamesRouter;
+     
