17294334cb739180f374825489a538ecc01962c8
nikitapoludencev <poludencevnikita@gmail.com>
2024-05-20 02:42:51 +0300

    Добавлены заготовки для удаления игры по id и добавления новой игры

--- routes/games.js
+++ routes/games.js
@@ -4,8 +4,17 @@
  
      };
      
+     const deleteGame = async (req, res) => {
+
+     };
+
+     const addGame = async (req, res) => {
+
+     };
      
      gamesRouter.get("/games", getAllGames)
+     gamesRouter.delete("/games/:id", deleteGame);
+     gamesRouter.post("/games", addGame);
      
      module.exports = gamesRouter;
      
