cea58eafc881d962b2b5eb60a28fc9633012440e
nikitapoludencev <poludencevnikita@gmail.com>
2024-05-20 21:41:12 +0300

    Добавлен CORS

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const gamesRoute = require('./routes/games')
 
 const bodyParser = require("body-parser")
 
+const cors = require("./middlewares/cors")
+
 const PORT = 3000;
 const app = express();
 
+app.use(cors, bodyParser.json());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mainRoute, gamesRoute);
---
+++ middlewares/cors.js
@@ -0,0 +1,14 @@
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    const allowedCors = [
+        "localhost:3000",
+        "https://practicum.yandex.ru",
+        "https://students-projects.ru",
+    ];
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    next();
+}; 
+
+module.exports = cors;
